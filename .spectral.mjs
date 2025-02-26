8c779dd3b48e78abfb195dacde895514e2f24eb4
devops@stoplight.io <devops@stoplight.io>
2025-02-26 15:10:37 +0530

    Added .spectral.mjs

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-19902.stoplight-dev.com/api/v1/projects/cHJqOjcyNjM/spectral.js?branch_slug=main&token=9ddfb633-f0da-48de-8deb-efaf9b895737";
+export default { extends: ruleset };
